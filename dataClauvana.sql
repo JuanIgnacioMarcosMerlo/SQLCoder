06d1a18a5276b30766021127a31936ad9d9c3f94
Juan Ignacio Marcos Merlo <128730627+JuanIgnacioMarcosMerlo@users.noreply.github.com>
2023-09-06 21:41:00 -0300

    agregado datos a mi db

--- dataClauvana.sql
+++ dataClauvana.sql
@@ -1,2 +1,18 @@
-select * 
-from prenda
+insert into fechas ( Fecha )
+value ('2023-09-06');
+
+insert into prenda ( Fecha, Id_Prenda, Stock_total )
+value 	('2023-09-06', 'Angeles',30),
+		('2023-09-06', 'Silvana',40),
+		('2023-09-06', 'Claudia',50),
+        ('2023-09-06', 'Angie',80),
+        ('2023-09-06', 'Delfina',30);
+
+
+clientesINSERT INTO precio (Fecha, Id_Prenda, Precio_Mayorista, Precio_Minorista)
+value
+	('2023-09-06','Angeles',1500,1800),
+    ('2023-09-06','Angie',6000,7200),
+    ('2023-09-06','Delfina',1500,1800),
+    ('2023-09-06','Silvana',1500,1800),
+    ('2023-09-06','Claudia',1500,1800);
